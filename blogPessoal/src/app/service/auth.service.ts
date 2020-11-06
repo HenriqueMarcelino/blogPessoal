ad7071db2293762f4703506297d6d305165e1894
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2020-11-05 21:30:17 -0300

    implementação de componentes / validação de senha

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -11,10 +11,31 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   logar(userLogin: UserLogin){
-    return this.http.post('http://localhost:9000/usuarios/logar', UserLogin)
+    return this.http.post('http://localhost:8080/usuarios/logar', UserLogin)
   }
 
   cadastrar(user:User){
-    return this.http.post('http://localhost:9000/usuarios/cadastrar', user)
+    return this.http.post('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  btnSair(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if( token != null){
+      ok = true
+    }
+    
+    return ok
+  }
+
+  btnLogin(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if ( token == null){
+      ok = true
+    }
+
   }
 }
